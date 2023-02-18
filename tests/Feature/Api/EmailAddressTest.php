ccabbb908234a9a3b804d4045b1a8e8363d02c5a
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-02-18 16:59:15 -0500

    Add inbox and label test

---
+++ tests/Feature/Api/EmailAddressTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Models\Label;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+it('can list labels', function () {
+    Label::factory()->count(10)->create();
+
+    $label = Label::orderBy('name')->first();
+
+    $this->get(route('labels.index'))
+        ->assertJson(fn(AssertableJson $json) => $json->has('data', 10)
+            ->has('data.0', fn(AssertableJson $json) => $json->where('id', $label->id)
+                ->where('name', $label->name)
+                ->where('color', $label->color)
+                ->etc()
+            )
+        );
+});
+
+it('can limit labels', function () {
+    Label::factory()->count(10)->create();
+
+    $this->get(route('labels.index') . '?limit=5')
+        ->assertJson(fn(AssertableJson $json) => $json->has('data', 5));
+});
+
+it('can search for a label', function() {
+    Label::factory()->count(10)->create();
+    Label::factory()->create(['name' => 'Test Label']);
+
+    $this->get(route('labels.index') . '?search=Test')
+        ->assertJson(fn(AssertableJson $json) => $json->has('data', 1)
+            ->has('data.0', fn (AssertableJson $json) =>
+            $json->where('name', 'Test Label')
+                ->etc()
+            )
+        );
+});
+
+it('can show a single label', function () {
+    $label = Label::factory()->create();
+
+    $this->get(route('labels.show', $label))
+        ->assertJson([
+            'data' => [
+                'id' => $label->id,
+                'name' => $label->name,
+            ],
+        ]);
+});
