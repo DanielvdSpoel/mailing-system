128c376aa22cbd17c7db1f015f48c84e8b43b244
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-02-18 23:37:28 -0500

    Another round of laravel pint

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -29,8 +29,6 @@ class EventServiceProvider extends ServiceProvider
 
     /**
      * Register any events for your application.
-     *
-     * @return void
      */
     public function boot(): void
     {
