603fc4da9180c522b3e1c4f7d1e9fc92adaadb08
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-02-18 22:35:00 -0500

    Add back laravel cors middleware

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,21 +10,12 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
-
-    /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
         $schedule->command('telescope:prune --hours=48')->daily();
 
@@ -40,7 +31,7 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
